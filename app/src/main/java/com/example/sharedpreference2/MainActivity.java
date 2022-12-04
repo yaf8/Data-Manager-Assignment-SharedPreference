c92f74e52e932a418f18d5ffb762bdd0d02440e4
Yafet Abebe <yafetema15@gmail.com>
2022-12-04 03:38:31 +0300

    update 1.1

--- app/src/main/java/com/example/sharedpreference2/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference2/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.sharedpreference2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSave, btnDelete, btnDeleteAll, btnUpdate, btnSearch, btnDisplay;
+    EditText edtID, edtFname, edtLname;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSave = findViewById(R.id.btnSave);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnDeleteAll = findViewById(R.id.btnDeleteAll);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnSearch = findViewById(R.id.btnSearch);
+        btnDisplay = findViewById(R.id.btnDisplay);
+        edtID = findViewById(R.id.EditTextID);
+        edtFname = findViewById(R.id.EditTextFirstName);
+        edtLname = findViewById(R.id.EditTextLastName);
+
+        btnSave.setOnClickListener(v -> {
+
+        });
+
+        btnDelete.setOnClickListener(v -> {
+
+        });
+
+        btnDeleteAll.setOnClickListener(v -> {
+
+        });
+
+        btnUpdate.setOnClickListener(v -> {
+
+        });
+
+        btnSearch.setOnClickListener(v -> {
+
+        });
+
+        btnDisplay.setOnClickListener(v -> {
+
+        });
+
+
     }
 }
